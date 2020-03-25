84ce7cc363c3dfa5fdfe4891ed874fd79bec4e98
fares droubi <fares.droubi@gmail.com>
2020-03-25 14:59:44 +0100

    Structuring code example in directories.

---
+++ src/pages/date/Date_full-example.js
@@ -0,0 +1,32 @@
+//import React from "react"
+//import { graphql } from "gatsby"
+//
+//const Page = ({ data }) => {
+//  const document = data.prismic.allPages.edges[0].node
+//
+//	const date = Date(document.data.date);
+//	const formattedDate = Intl.DateTimeFormat('en-US',{
+//	  year: 'numeric',
+//	  month: 'short',
+//	  day: '2-digit' }).format(date);
+//	// Output in M d, Y format
+//
+//  return (
+//    <h3 className="date">${document.date}</h3>
+//  )
+//}
+//
+//export const query = graphql`
+//query{
+// prismic {
+//  allPages(uid: "date-page") {
+//		edges {
+//      node {
+//        release_date
+//      }
+//    }
+//  }
+//}
+//`
+//
+//export default Page
