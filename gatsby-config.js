5be4cd6fea4dea522aaf39cbf314aa397df0353a
fares droubi <fares.droubi@gmail.com>
2020-03-25 13:30:04 +0100

    Replacing the Prismic repo with a better name one

--- gatsby-config.js
+++ gatsby-config.js
@@ -24,13 +24,12 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
     {
       resolve: 'gatsby-source-prismic-graphql',
       options: {
-        repositoryName: 'prismic-gatsby-documentations-fdr',
+        repositoryName: 'prismic-gatsby-documentations',
         path: '/preview',
         previews: true,
         sharpKeys: [
