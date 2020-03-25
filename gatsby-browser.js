1017cbeb4ed4584a9fd75f4e03892163a963b853
fares droubi <fares.droubi@gmail.com>
2020-03-25 12:41:47 +0100

    Making the project usable by the education team (code cleaning, basic styles)

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,7 +1,4 @@
-/**
- * Implement Gatsby's Browser APIs in this file.
- *
- * See: https://www.gatsbyjs.org/docs/browser-apis/
- */
+const { registerLinkResolver } = require('gatsby-source-prismic-graphql');
+const { linkResolver } = require('./src/utils/linkResolver');
 
-// You can delete this file if you're not using it
+registerLinkResolver(linkResolver);
--- gatsby-config.js
+++ gatsby-config.js
@@ -1,8 +1,8 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
-    author: `@gatsbyjs`,
+    title: `Gatsby Prismic documentations`,
+    description: ``,
+    author: `@faresd`,
   },
   plugins: [
     `gatsby-plugin-react-helmet`,
@@ -27,8 +27,17 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
-    // this (optional) plugin enables Progressive Web App + Offline functionality
-    // To learn more, visit: https://gatsby.dev/offline
-    // `gatsby-plugin-offline`,
+    {
+      resolve: 'gatsby-source-prismic-graphql',
+      options: {
+        repositoryName: 'prismic-gatsby-documentations-fdr',
+        path: '/preview',
+        previews: true,
+        sharpKeys: [
+          /image|photo|picture/, // (default)
+          'profilepic',
+        ],
+      }
+    }
   ],
 }
