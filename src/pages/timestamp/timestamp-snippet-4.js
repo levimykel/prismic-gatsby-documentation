a21ba73e48e25f3cd04252891b2a82635304c1b3
fares droubi <fares.droubi@gmail.com>
2020-03-30 17:05:16 +0200

    update

---
+++ src/pages/timestamp/timestamp-snippet-4.js
@@ -0,0 +1,37 @@
+import React from "react"
+import { graphql } from "gatsby"
+import { Date } from 'prismic-reactjs'
+
+const Page = ({ data }) => {
+  const document = data.prismic.allPages.edges[0].node
+
+  const timestamp = Date(document.event_date)
+
+  var formattedTimestamp = Intl.DateTimeFormat('en-US',{
+    year: "numeric",
+    month: "short",
+    day: "2-digit",
+    hour: "numeric",
+    minute: "2-digit",
+    second: "2-digit"
+  }).format(timestamp)
+// Outputs as "Mar 24, 2020, 12:00:00 AM"
+
+  return formattedTimestamp
+}
+
+export const query = graphql`
+query {
+  prismic {
+    allPages(uid: "test-page") {
+      edges {
+        node {
+          publication_date
+        }
+      }
+    }
+  }
+}
+`
+
+export default Page
