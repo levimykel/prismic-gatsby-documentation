25bef86f05c1aace84fb5b834baa20a422062867
fares droubi <fares.droubi@gmail.com>
2020-06-24 16:27:53 +0200

    Changes to LinkResolver to work with Prismic Link and previews.

--- src/utils/linkResolver.js
+++ src/utils/linkResolver.js
@@ -1,7 +1,11 @@
 module.exports = {
   linkResolver(doc) {
+    const properties = doc._meta || doc
     if (doc.uid === "number-page") {
-      return `/rendering/number/number-full-example`
-    } else return '/'
-  }
+      return '/rendering/number/number-full-example'
+    } if (doc.type === 'page') {
+      return `/${properties.uid}`
+    }
+    return '/'
+  },
 }
